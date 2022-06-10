5aac738d32bffcf8fe6502f8273687c21cbcc025
Anggit Mochammad <anggitmochammad@gmail.com>
2022-06-10 13:51:20 +0700

    model and migration penjualan

---
+++ app/Models/Penjualan.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class Penjualan extends Eloquent
+{
+    use HasFactory;
+
+    protected $connection = 'mongodb';
+    protected $collection = 'penjualan';
+
+    protected $fillable = [
+        'id_kendaraan',
+        'jumlah',
+    ];
+
+    
+}
